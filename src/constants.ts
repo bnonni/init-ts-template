3feff4acffcfeb0a2e2f85708a529108bcfaf4ba
bnonni <github@nonni.dev>
2025-03-03 15:55:27 -0500

    reinit repo at v0.8.2

---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+export const SOFTWARE_LICENSES = new Map<string, string>(
+  [
+    ['AGPL-3.0'     , 'GNU Affero General Public License v3.0'],
+    ['Apache-2.0'   , 'Apache License 2.0'],
+    ['BSD-2-Clause' , 'BSD 2-Clause "Simplified" License'],
+    ['BSD-3-Clause' , 'BSD 3-Clause "New" or "Revised" License'],
+    ['BSL-1.0'      , 'Boost Software License 1.0'],
+    ['CC0-1.0'      , 'Creative Commons Zero v1.0 Universal'],
+    ['EPL-2.0'      , 'Eclipse Public License 2.0'],
+    ['GPL-2.0'      , 'GNU General Public License v2.0'],
+    ['GPL-3.0'      , 'GNU General Public License v3.0'],
+    ['LGPL-2.1'     , 'GNU Lesser General Public License v2.1'],
+    ['MIT'          , 'MIT License'],
+    ['MPL-2.0'      , 'Mozilla Public License 2.0'],
+    ['Unlicense'    , 'The Unlicense']
+  ]
+);
